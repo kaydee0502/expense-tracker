2bfab318e82f50372abfab924fc03b283c10bcbe
kaydee0502 <kshitijdhama513@gmail.com>
2025-06-05 07:55:10 +0000

    config changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Empty config for now
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+    V1_NEXT_PUBLIC_EXPENSE_API_ENDPOINT: process.env.V1_NEXT_PUBLIC_EXPENSE_API_ENDPOINT,
+    V1_NEXT_PUBLIC_USER_API_ENDPOINT: process.env.V1_NEXT_PUBLIC_USER_API_ENDPOINT,
+    V1_NEXT_PUBLIC_AUTH_API_ENDPOINT: process.env.V1_NEXT_PUBLIC_AUTH_API_ENDPOINT,
+  }
 }
 
 module.exports = nextConfig
