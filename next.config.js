78306a4437ebcaa59ee62210b1da119c7f3d1959
kaydee0502 <kshitijdhama513@gmail.com>
2025-06-05 08:44:42 +0000

    Revert config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  },
-  env: {
-    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
-    V1_NEXT_PUBLIC_EXPENSE_API_ENDPOINT: process.env.V1_NEXT_PUBLIC_EXPENSE_API_ENDPOINT,
-    V1_NEXT_PUBLIC_USER_API_ENDPOINT: process.env.V1_NEXT_PUBLIC_USER_API_ENDPOINT,
-    V1_NEXT_PUBLIC_AUTH_API_ENDPOINT: process.env.V1_NEXT_PUBLIC_AUTH_API_ENDPOINT,
-  }
+  // Empty config for now
 }
 
 module.exports = nextConfig
